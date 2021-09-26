05ce061d375b892b74642f5c1614a6913724d079
unknown <naimul8080@gmail.com>
2021-09-26 19:07:19 +0600

    added readme.md file

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -6,7 +6,7 @@ const Header = () => {
         <div className="text-center header">
             <h2>Minister of Bangladesh</h2>
             <h5>Announce the all minister of Bangladesh 2020-2025</h5>
-            <h3>Total Minister : 10</h3>
+            <h3>Total Budget of Salary (monthly) : 100K</h3>
         </div>
     );
 };
